95ca4f0c6f7a5630ab95fbf07ada42ef2b08201c
Fernando Mayo <fermayo@gmail.com>
2024-06-04 10:23:42 +0200

    Update array.test.js

--- tests/array.test.js
+++ tests/array.test.js
@@ -23,8 +23,7 @@ describe('Array Utilities', () => {
     expect(calculateAverage([10, 20, 30])).toBe(20);
   });
 
-  // **EARLY FLAKE DETECTION**: uncomment this test to test Early Flake Detection
-  // test('flakyIsNull should return true if the value is null', () => {
-  //   expect(flakyIsNull(null)).toBe(true);
-  // });
+  test('flakyIsNull should return true if the value is null', () => {
+    expect(flakyIsNull(null)).toBe(true);
+  });
 });
